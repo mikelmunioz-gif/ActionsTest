90a8ac95075dc8fd669f872f91dd55a76aa990fa
Mikel Muñoz <mikel.munioz@pers.eus>
2025-09-23 14:52:18 +0200

    Mejora

---
+++ accionestest/__init__.py
@@ -0,0 +1,2 @@
+from .math_utils import add, divide
+__all__ = ["add", "divide"]
---
+++ scripts/saluda.py
@@ -0,0 +1,9 @@
+from accionestest import add, divide
+
+def main() -> None:
+    print("Hola desde Python 👋")
+    print("2 + 3 =", add(2, 3))
+    print("10 / 2 =", divide(10, 2))
+
+if __name__ == "__main__":
+    main()
