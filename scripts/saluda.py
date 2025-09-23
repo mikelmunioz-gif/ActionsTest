1e81f45d64188b9c3673465ebcbc641a47e5cb1e
Mikel Muñoz <mikel.munioz@pers.eus>
2025-09-23 15:44:06 +0200

    Modificación

--- scripts/saluda.py
+++ scripts/saluda.py
@@ -1,7 +1,7 @@
 from accionestest import add, divide
 
 def main() -> None:
-    print("Hola desde Python 👋")
+    print("Hola desde Python. Esto es una prueba 👋")
     print("2 + 3 =", add(2, 3))
     print("10 / 2 =", divide(10, 2))
 
